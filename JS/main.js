cb902ce3834de9c8329ea2d9bbb4ac08a80bf2f6
ccayiyaaa <adellacalistaa13@gmail.com>
2025-03-19 09:18:41 +0700

    [FEAT] first commit

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+//console.log("hai hola annyeong");
+//document.write("neomu chuayoo")
+
+let namaAja = "adella" //camelCase
+console.log(namaAja);
+
+namaAja = 123456
+console.log(namaAja);
+console.log(typeof(namaAja));
+
+
+
+var namalengkap
+//const nama2
